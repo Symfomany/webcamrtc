932b1c5edd3292ab7d6480a23f39398e970d8c39
Julien Taiwa <taiwa.julien@gmail.com>
2022-08-10 09:56:54 +0200

    ok

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -34,17 +34,12 @@ pc.onaddstream = function(obj) {
     vid.setAttribute('autoplay', 'autoplay');
     vid.setAttribute('id', 'video-small');
     document.getElementById('users-container').appendChild(vid);
-    //vid.src = window.URL.createObjectURL(obj.stream);
-    vid.src = obj.stream;
-    vid.play();
+    vid.src = window.URL.createObjectURL(obj.stream);
 };
 
 navigator.getUserMedia({ video: true }, function(stream) {
     var video = document.querySelector('video');
-    // video.src = window.URL.createObjectURL(stream);
-    video.src = stream;
-    // video.play();
-
+    video.src = window.URL.createObjectURL(stream);
     pc.addStream(stream);
 }, error);
 
